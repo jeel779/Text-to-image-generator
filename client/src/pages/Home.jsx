c84ce66971e139772fc26644104618a2de2bfbe4
Jeel Patel <jeelpatel779@gmail.com>
2025-12-11 15:25:36 +0530

    added steps

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Header from '../components/Header'
+import Steps from '../components/Steps'
 
 const Home = () => {
   return (
-    <Header/>
+    <> 
+      <Header/>
+      <Steps/>
+    </>
   )
 }
 
